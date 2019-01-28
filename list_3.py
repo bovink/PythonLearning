5bcc2f3f7d75e252b3e930d2f1f1e958599abd3f
bovink <bovink@sina.com>
2019-01-28 14:55:20 +0800

    列表

--- list_3.py
+++ list_3.py
@@ -1,4 +1,4 @@
-# 第一种
+# 单循环
 squares = []
 for x in range(10):
     squares.append(x ** 2)
@@ -9,3 +9,14 @@ print(squares)
 # 第三种
 squares = [x ** 2 for x in range(10)]
 print(squares)
+
+# 双重循环
+list = [(x, y) for x in [1, 2, 3] for y in [2, 5, 7] if x != y]
+print(list)
+# 等价于
+list = []
+for x in [1, 2, 3]:
+    for y in [2, 5, 7]:
+        if x != y:
+            list.append((x, y))
+print(list)
