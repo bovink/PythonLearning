5b8614a72d36c6ae382ac490785e0b83217f2c19
bovink <bovink@sina.com>
2019-01-28 15:21:58 +0800

    列表

--- list_3.py
+++ list_3.py
@@ -1,22 +1,20 @@
-# 单循环
-squares = []
-for x in range(10):
-    squares.append(x ** 2)
-print(squares)
-# 第二种
-squares = list(map(lambda x: x ** 2, range(10)))
-print(squares)
-# 第三种
-squares = [x ** 2 for x in range(10)]
-print(squares)
+# 可以用简洁的语法创建列表
+# 首先是控制语句的执行顺序，按照从左往右的顺序去理解
 
-# 双重循环
-list = [(x, y) for x in [1, 2, 3] for y in [2, 5, 7] if x != y]
+# 单个循环
+list = [x for x in range(10)]
 print(list)
-# 等价于
-list = []
-for x in [1, 2, 3]:
-    for y in [2, 5, 7]:
-        if x != y:
-            list.append((x, y))
+# 多个循环
+list = [(x, y) for x in range(-3, 0) for y in range(0, 3)]
+print(list)
+# 添加条件语句
+list = [(x, y) for x in range(-3, 0) for y in range(0, 3) if abs(x) != abs(y)]
+print(list)
+
+# 其次遍历出来的元素可以对其执行方法，包括多重嵌套方法，如果其本身包含可执行的方法也可以执行
+list = [abs(x) for x in range(-4, 5)]
+print(list)
+list = [str(abs(x)) for x in range(-4, 5)]
+print(list)
+list = [x.strip() for x in ['aaaaa ', ' bbbbb', ' cc c cc ']]
 print(list)
