133353eae153427e5d44a4db5554751fed8cdd84
bovink <bovink@sina.com>
2019-04-19 10:30:31 +0800

    添加QMessageBox代码

--- widget/QMessageBox.py
+++ widget/QMessageBox.py
@@ -16,14 +16,21 @@ class Window(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
 
-        buttonReply = QMessageBox.question(self, 'Message ', 'Do you like PyQt5', QMessageBox.Yes, QMessageBox.No)
+        button = QPushButton('click', self)
+        button.clicked.connect(self.clickBtn)
+        button.setToolTip('click btn show console msg')
+        button.setGeometry(0, 0, 100, 100)
+        button.move(button.geometry().width(), button.geometry().height())
+        button.show()
+
+    def clickBtn(self):
+        buttonReply = QMessageBox.question(self, 'Message', 'Do you like PyQt5', QMessageBox.Yes, QMessageBox.No)
         if buttonReply == QMessageBox.Yes:
             print('yes')
         else:
             print('no')
-
-        self.show()
 
 
 if __name__ == '__main__':
