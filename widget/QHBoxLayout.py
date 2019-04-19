4346c5c07055c8576064378ee3b330a24a1269ca
bovink <bovink@sina.com>
2019-04-19 17:05:36 +0800

    添加QHBoxLayout和QVBoxLayout代码

---
+++ widget/QHBoxLayout.py
@@ -0,0 +1,38 @@
+from PyQt5.QtWidgets import *
+import sys
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        self.left = 100
+        self.top = 100
+        self.width = 1024
+        self.height = 720
+        self.title = 'hello world'
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+        # 水平排列和垂直排列
+        layout = QVBoxLayout(self)
+        # layout = QHBoxLayout(self)
+
+        btn1 = QPushButton('btn1')
+        btn2 = QPushButton('btn2')
+        btn3 = QPushButton('btn3')
+        btn4 = QPushButton('btn4')
+        layout.addWidget(btn1)
+        layout.addWidget(btn2)
+        layout.addWidget(btn3)
+        layout.addWidget(btn4)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Window()
+    sys.exit(app.exec_())
