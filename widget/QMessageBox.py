39d18e33bd6b7cbfe6e27cb74612e179dc1d13f8
bovink <bovink@sina.com>
2019-04-19 10:26:12 +0800

    创建QMessageBox

---
+++ widget/QMessageBox.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import *
+import sys
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        self.left = 100
+        self.top = 100
+        self.width = 1024
+        self.height = 720
+        self.title = 'hello world'
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        buttonReply = QMessageBox.question(self, 'Message ', 'Do you like PyQt5', QMessageBox.Yes, QMessageBox.No)
+        if buttonReply == QMessageBox.Yes:
+            print('yes')
+        else:
+            print('no')
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Window()
+    sys.exit(app.exec_())
