ebce47058c1eee216d31b7c1d5816effe423e022
bovink <bovink@sina.com>
2019-04-19 11:44:11 +0800

    添加QMenu代码

---
+++ widget/QMenu.py
@@ -0,0 +1,41 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+import sys
+
+
+class Window(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.left = 100
+        self.top = 100
+        self.width = 1024
+        self.height = 720
+        self.title = 'hello world'
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('File')
+        editMenu = mainMenu.addMenu('Edit')
+        viewMenu = mainMenu.addMenu('View')
+        searchMenu = mainMenu.addMenu('Search')
+        toolsMenu = mainMenu.addMenu('Tools')
+        helpMenu = mainMenu.addMenu('Help')
+
+        exitButton = QAction(QIcon('/myresource/cookie.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Window()
+    sys.exit(app.exec_())
