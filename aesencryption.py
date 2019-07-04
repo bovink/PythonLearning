5f5e531e0c9298f6b746fd20bbd9d11fe9dff5f3
bovink <bovink@sina.com>
2019-07-04 15:41:30 +0800

    python使用aes加密的后的数据，在c++中成功恢复

--- aesencryption.py
+++ aesencryption.py
@@ -4,19 +4,31 @@ from Crypto import Random
 from Crypto.Protocol.KDF import PBKDF2
 import base64
 
+PADDING = b'{'
 BLOCK_SIZE = 16
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE).encode(encoding="utf-8")
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 key = '1234567890123456'
 iv = '1234567890123456'
 
 obj = AES.new(key, AES.MODE_CBC, iv)
-msg = 'something encrypt by python'
-chphier = obj.encrypt(pad(msg))
+# 获取图片数据
+image = open('button.png', 'rb')
+content = image.read()
+# content = b'this is encrypt by python'
+image.close()
+print(content)
+print(len(content))
+content = pad(content)
+print(content)
+print(len(content))
+chphier = obj.encrypt(content)
 print(base64.b64encode(chphier))
-print(len(chphier))
-print(chphier)
+
+obj2 = AES.new(key, AES.MODE_CBC, iv)
+x = obj2.decrypt(chphier)
+print(unpad(x))
 
 f = open('test.enc', 'wb')
 f.write(chphier)
