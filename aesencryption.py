343cfe1075251bbff87b16c02485779bbe71ca77
bovink <bovink@sina.com>
2019-07-04 11:02:43 +0800

    python使用aes加密的后的数据，在c++中成功恢复

--- aesencryption.py
+++ aesencryption.py
@@ -8,43 +8,17 @@ BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
-key = '123'
-key = pad(key)
-iv = Random.new().read(16)
-# iv = pad(iv)
-obj = AES.new(key, AES.MODE_CFB, iv)
+key = '1234567890123456'
+iv = '1234567890123456'
 
-# message = "The answer is no"
-# 读图
-file = open('page10_atlas0_2.png', 'rb')
-filecontent = file.read()
-file.close()
+obj = AES.new(key, AES.MODE_CBC, iv)
+msg = 'something encrypt by python'
+chphier = obj.encrypt(pad(msg))
+print(base64.b64encode(chphier))
+print(len(chphier))
+print(chphier)
 
-ciphertext = obj.encrypt(filecontent)
-print(ciphertext)
+f = open('test.enc', 'wb')
+f.write(chphier)
+f.close()
 
-f2 = open('a.txt', 'wb')
-# f2.write(base64.b64encode(iv+ciphertext))
-f2.write(iv + ciphertext)
-f2.close()
-
-# obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-# a = obj2.decrypt(ciphertext)
-# print(a)
-# print(type(a))
-
-# 存图
-s = open('a.txt', 'rb')
-sc = s.read()
-s.close()
-
-# h = base64.b64decode(sc)
-iv = sc[:16]
-aa = sc[16:]
-obj2 = AES.new(key, AES.MODE_CFB, iv)
-
-n = obj2.decrypt(aa)
-
-f2 = open('a.png', 'wb')
-f2.write(n)
-f2.close()
