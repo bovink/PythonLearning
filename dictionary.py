86e6076844d92298a4a88c7c2540b609f2fb6aab
bovink <bovink@sina.com>
2019-07-08 14:16:06 +0800

    根据索引筛选出免费和收费页面资源。

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,39 @@
-# 创建字典
-myDict = {}
-print(myDict)
+import os
+from os import listdir
 
-# 字典添加键值对
-myDict['name'] = 'lilei'
-print(myDict)
+# 资源路径
+resource_path = 'time/waste1'
 
-m = myDict.get('name')
-i = myDict.items()
-print(i)
+# 免费页面索引
+free_pages = [1, 2]
+
+# 免费页面过滤
+free_pages_filter = []
+
+# 免费页面
+free_pages_name = []
+
+# 收费页面
+purchase_pages_name = []
+
+for i in free_pages:
+    free_pages_filter.append('page' + str(i) + '_')
+    free_pages_filter.append('page' + str(i) + '.fui')
+
+print(free_pages_filter)
+
+for file in listdir(resource_path):
+    for f in free_pages_filter:
+        if f in file:
+            if file not in free_pages_name:
+                free_pages_name.append(file)
+
+purchase_pages_name = set(listdir(resource_path)).difference(set(free_pages_name))
+print('====================free page:%d====================' % free_pages_name.__len__())
+for i in free_pages_name:
+    print(i)
+
+print('====================purchase page:%d====================' % purchase_pages_name.__len__())
+
+for i in purchase_pages_name:
+    print(i)
