88930ce59b2e678d4b487e238689a162e9c65220
bovink <bovink@sina.com>
2019-07-23 21:35:49 +0800

    优化Kindle截图程序

--- kindle_snapshot.py
+++ kindle_snapshot.py
@@ -8,19 +8,24 @@ import win32gui
 import time
 from PIL import ImageGrab
 from win32.lib import win32con
+import os
 
-handle = win32gui.FindWindow(None, 'bovink的 Kindle for PC - 麻雀テクニック')
+name = '麻雀の失敗学 (近代麻雀戦術シリーズ)'
+dir = win32con.VK_LEFT
+pages = 196
+os.makedirs(name)
+handle = win32gui.FindWindow(None, 'bovink的 Kindle for PC - ' + name)
 if handle > 0:
     print('存在')
 
-    path = 'G:\Project\PyCharmProjects\PythonLearning\pic'
-    it  = iter(range(0, 260))
+    path = 'G:\Project\PyCharmProjects\PythonLearning\\' + name
+    it = iter(range(0, pages))
     for i in it:
         im = ImageGrab.grab()
         file = path + '\\image' + str(i + 1) + '.jpg'
         im.save(file, 'jpeg')
-        win32gui.SendMessage(handle, win32con.WM_KEYDOWN, win32con.VK_LEFT, 0)
-        win32gui.SendMessage(handle, win32con.WM_KEYUP, win32con.VK_LEFT, 0)
+        win32gui.SendMessage(handle, win32con.WM_KEYDOWN, dir, 0)
+        win32gui.SendMessage(handle, win32con.WM_KEYUP, dir, 0)
         time.sleep(0.25)
 
 
