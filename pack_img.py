c8095f19e7bd86c9a3b94aad126bd42a193716b4
bovink <bovink@sina.com>
2019-08-07 09:58:21 +0800

    打包表情

---
+++ pack_img.py
@@ -0,0 +1,25 @@
+
+import os
+from os import listdir
+
+root_path = 'bp'
+subdir = os.listdir(root_path)
+subdir.sort()
+print(subdir)
+
+########命令
+image_format = '--opt RGBA4444 '
+max_size = '--max-size 4096 '
+output_data = '--data output/'
+output_data_suffix = '.plist '
+output_sheet = '--sheet output/'
+output_sheet_suffix = '.pvr.ccz '
+
+
+filter_file = '/*.png '
+
+for i in subdir:
+    index = subdir.index(i)
+    name = 'bp'+str(index)
+    command = 'TexturePacker '+image_format+max_size+output_data+name+output_data_suffix+output_sheet+name+output_sheet_suffix+root_path+'/'+i+filter_file
+    os.system(command)
